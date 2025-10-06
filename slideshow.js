2e89354e2f9b8668d746f56ea7bef0b69363ca9f
crabbster-roll <mortena@email.sc.edu>
2025-10-06 19:07:17 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,13 +1,14 @@
+// slideshow.js
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let slides = document.getElementsByClassName("slides");
+  const slides = document.getElementsByClassName("slides");
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // change image every 4 seconds
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
