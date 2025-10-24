6ff9a89291cd820b3f7b1b91023136b134414a5e
crabbster-roll <mortena@email.sc.edu>
2025-10-24 15:51:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Social Media Sharing Functions
   function shareOnFacebook() {
-    // Use your actual website link here
     const urlToShare = 'https://crabbster-roll.github.io/ComfortCreature-Website/';
     const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
     window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
   }
 
   function shareOnTwitter() {
-    // Use your actual website link and message here
     const urlToShare = 'https://crabbster-roll.github.io/ComfortCreature-Website/';
     const textToShare = 'Check out Creature Comfort!';
     const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
-    window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+    window.open(twitterShareUrl, 'Share on X (Twitter)', 'width=600,height=400');
   }
 
-  // Event listeners for buttons
   const shareFacebookButton = document.getElementById("share-facebook");
   const shareTwitterButton = document.getElementById("share-twitter");
 
